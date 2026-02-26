c0bc9de21fe0bb72536bca92da6b655808a830ff
CarlosCESSLenovo <carlosdudus3412@gmail.com>
2026-02-26 09:54:03 -0400

    Organizanddo o docker

--- backend/merenda_back/merenda_back/settings.py
+++ backend/merenda_back/merenda_back/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'rest_framework', # Obrigatoriamente, temos que colocar o rest_framework
 ]
 
+# Configuração de autenticação JWT
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
